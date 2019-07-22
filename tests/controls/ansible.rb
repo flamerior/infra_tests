5e165a1226b75c332541282df2059ac63d7eec12
anechaev <anechaev@plesk.com>
2019-07-22 16:30:53 +0700

    add terraform get for tflint

--- tests/controls/ansible.rb
+++ tests/controls/ansible.rb
@@ -11,13 +11,16 @@ control 'ansible' do
     its('exit_status') { should eq 0 }
   end
 
-  describe command('cd ansible && ansible-lint playbooks/site.yml --exclude=roles/jdauphant.nginx') do
-    its('stdout') { should eq '' }
-    its('exit_status') { should eq 0 }
+  Dir.glob('ansible/playbooks/*.yml') do |playbook|
+    describe command('cd ansible && ansible-lint playbooks/' + playbook + '.yml --exclude=roles/jdauphant.nginx && cd ..') do
+      its('stdout') { should eq '' }
+      its('exit_status') { should eq 0 }
+    end
   end
 
   describe command('find ansible/playbooks -name "*.yml" -type f -print0 | ANSIBLE_ROLES_PATH=ansible/roles xargs -0 -n1 ansible-playbook --syntax-check') do
     its('stderr') { should_not match (/The error appears to have been/) }
     its('exit_status') { should eq 0 }
   end
+
 end

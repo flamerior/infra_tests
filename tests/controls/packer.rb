f7321ce46911b2cebc50e4ff178578c879091fad
anechaev <anechaev@plesk.com>
2019-07-22 15:43:33 +0700

    add terraform validate

--- tests/controls/packer.rb
+++ tests/controls/packer.rb
@@ -5,34 +5,7 @@ title 'packer validation'
 
 control 'packer' do
   impact 1
-  title 'Run packer validate'
-
-  # files = [
-  #   'packer/app.json',
-  #   'packer/db.json',
-  #   'packer/variables.json.example',
-  #   'ansible/playbooks/packer_app.yml',
-  #   'ansible/playbooks/packer_db.yml',
-  # ]
-  #
-  # files.each do |path|
-  #   describe file(path) do
-  #     it { should exist }
-  #     its('content') { should match(%r{\n\Z}) }
-  #   end
-  # end
-  #
-  # describe command('packer validate -var-file=packer/variables.json.example packer/app.json') do
-  #   its('stdout') { should eq "Template validated successfully.\n" }
-  #   its('stderr') { should eq '' }
-  #   its('exit_status') { should eq 0 }
-  # end
-  #
-  # describe command('packer validate -var-file=packer/variables.json.example packer/db.json') do
-  #   its('stdout') { should eq "Template validated successfully.\n" }
-  #   its('stderr') { should eq '' }
-  #   its('exit_status') { should eq 0 }
-  # end
+  title 'Run packer validate for all templates'
 
   Dir.glob('packer/*.json') do |packerfile|
     describe command('packer validate -var-file=packer/variables.json.example '+ packerfile) do

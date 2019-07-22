d8eb37b23db7feccfd5fcc33d6387e3e83a0bcea
anechaev <anechaev@plesk.com>
2019-07-22 14:45:44 +0700

    test packer validate

--- tests/controls/packer.rb
+++ tests/controls/packer.rb
@@ -7,30 +7,39 @@ control 'packer' do
   impact 1
   title 'Run packer validate'
 
-  files = [
-    'packer/app.json',
-    'packer/db.json',
-    'packer/variables.json.example',
-    'ansible/playbooks/packer_app.yml',
-    'ansible/playbooks/packer_db.yml',
-  ]
+  # files = [
+  #   'packer/app.json',
+  #   'packer/db.json',
+  #   'packer/variables.json.example',
+  #   'ansible/playbooks/packer_app.yml',
+  #   'ansible/playbooks/packer_db.yml',
+  # ]
+  #
+  # files.each do |path|
+  #   describe file(path) do
+  #     it { should exist }
+  #     its('content') { should match(%r{\n\Z}) }
+  #   end
+  # end
+  #
+  # describe command('packer validate -var-file=packer/variables.json.example packer/app.json') do
+  #   its('stdout') { should eq "Template validated successfully.\n" }
+  #   its('stderr') { should eq '' }
+  #   its('exit_status') { should eq 0 }
+  # end
+  #
+  # describe command('packer validate -var-file=packer/variables.json.example packer/db.json') do
+  #   its('stdout') { should eq "Template validated successfully.\n" }
+  #   its('stderr') { should eq '' }
+  #   its('exit_status') { should eq 0 }
+  # end
 
-  files.each do |path|
-    describe file(path) do
-      it { should exist }
-      its('content') { should match(%r{\n\Z}) }
+  Dir.glob('packer/*.json') do |packerfile|
+    describe command('packer validate -var-file=packer/variables.json.example '+ packerfile) do
+      its('stdout') { should eq "Template validated successfully.\n" }
+      its('stderr') { should eq '' }
+      its('exit_status') { should eq 0 }
     end
   end
 
-  describe command('packer validate -var-file=packer/variables.json.example packer/app.json') do
-    its('stdout') { should eq "Template validated successfully.\n" }
-    its('stderr') { should eq '' }
-    its('exit_status') { should eq 0 }
-  end
-
-  describe command('packer validate -var-file=packer/variables.json.example packer/db.json') do
-    its('stdout') { should eq "Template validated successfully.\n" }
-    its('stderr') { should eq '' }
-    its('exit_status') { should eq 0 }
-  end
 end
